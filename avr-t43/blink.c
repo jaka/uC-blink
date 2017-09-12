9063e05fba05128915a2ba15c2ba581939189f21
jaka <jaka@live.jp>
2017-09-12 12:52:49 +0200

    Added blink example for DDT2 ver 4.3

---
+++ avr-t43/blink.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+ISR (TIMER0_OVF_vect)
+{
+  PORTB ^= _BV(PB2);
+}
+
+void setup(void)
+{
+  TCCR0B |= _BV(CS02);
+
+  TCNT0 = 0;
+  TIMSK0 = _BV(TOIE0);
+
+  sei();
+}
+
+int main(void)
+{
+
+  DDRB = _BV(PB2); /* pin 2 port B */
+  setup();
+
+  for (;;)
+    sleep_mode();
+
+   return 0;
+}
