26786222e5c53d3ef17664ae1b0a30046675151d
jaka <jaka@live.jp>
2018-01-14 21:34:37 +0100

    STMF072RB

---
+++ stmf072rb/main.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+void on(void) {
+  *((uint32_t *)0x48000018) = 0x20;
+}
+
+void off(void) {
+  *((uint32_t *)0x48000018) = 0x200000;
+}
+
+#define DELAY 0xffff
+
+int main(void)
+{
+  volatile unsigned int i;
+
+  *((uint32_t *)0x40021014) |= 0x20000;
+  *((uint32_t *)0x48000000) |= 0x400;
+
+  while (1) {
+
+    on();
+    for (i = DELAY; i; i--);
+
+    off();
+    for (i = DELAY; i; i--);
+
+  }
+
+  return 0;
+}
